d8534c476a98ad81c4961dc0e6bd4975cd7a45c0
Andreea <totyo_deea@yahoo.com>
2022-07-06 23:42:53 -0400

    ui quizQuestionActivity

--- app/src/main/java/com/example/flagquiz/Constants.kt
+++ app/src/main/java/com/example/flagquiz/Constants.kt
@@ -7,7 +7,84 @@ object Constants {
         val q1 = Question(
             1,
             "What country does this flag belong to?",
-
+            R.drawable.argentina,
+            "Argentina", "Australia","Armenia","Austria",
+            1
         )
+        questionlist.add(q1)
+
+        val q2 = Question(
+            2,
+            "What country does this flag belong to?",
+            R.drawable.belgium,
+            "Germany", "Belgium","Italy","Netherlands",
+            2
+        )
+        questionlist.add(q2)
+
+        val q3 = Question(
+            3,
+            "What country does this flag belong to?",
+            R.drawable.brazil,
+            "Bulgaria", "Bolivia","Brazil","Argentina",
+            3
+        )
+        questionlist.add(q3)
+
+        val q4 = Question(
+            4,
+            "What country does this flag belong to?",
+            R.drawable.denmark,
+            "Denmark", "Sweden","Norway","Finland",
+            1
+        )
+        questionlist.add(q4)
+
+        val q5 = Question(
+            5,
+            "What country does this flag belong to?",
+            R.drawable.australia,
+            "Austria", "Australia","United Kingdom","U.S.A",
+            2
+        )
+        questionlist.add(q5)
+
+        val q6 = Question(
+            6,
+            "What country does this flag belong to?",
+            R.drawable.fiji_svg,
+            "Bora-Bora", "Tahiti","Madagascar","Fiji",
+            4
+        )
+        questionlist.add(q6)
+
+        val q7 = Question(
+            7,
+            "What country does this flag belong to?",
+            R.drawable.germany_svg,
+            "Belgium", "Germany","Spain","Colombia",
+            2
+        )
+        questionlist.add(q7)
+
+        val q8 = Question(
+            8,
+            "What country does this flag belong to?",
+            R.drawable.india,
+            "Sudan", "Iran","India","Hungary",
+            3
+        )
+        questionlist.add(q8)
+
+        val q9 = Question(
+            9,
+            "What country does this flag belong to?",
+            R.drawable.kuwait,
+            "Jordan", "Kuwait","India","Ireland",
+            2
+        )
+        questionlist.add(q9)
+
+        return questionlist
     }
 }
