26b8f75785b7d39e0ef456ebe9c1689099f10e9d
Andreea <totyo_deea@yahoo.com>
2022-07-07 16:14:34 -0400

    quizQuestionActivity finished

--- app/src/main/java/com/example/flagquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/flagquiz/QuizQuestionsActivity.kt
@@ -6,17 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
     private var mQuestionsList:ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
-
+    //create variables for each view in the layout
     private var progressBar : ProgressBar? = null
     private var tvProgress : TextView? = null
     private var tvQuestion : TextView? = null
@@ -48,31 +45,36 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionFour?.setOnClickListener(this)
         btnSubmit?.setOnClickListener(this)
 
-       mQuestionsList = Constants.getQuestions()
+        mQuestionsList = Constants.getQuestions()
 
         setQuestion()
-
-
     }
-
+    //setting the question to UI components.
     private fun setQuestion() {
-
+// Getting the question from the list with the help of current position.
         val question: Question = mQuestionsList!![mCurrentPosition - 1]
-        ivImage?.setImageResource(question.image)
-        progressBar?.progress = mCurrentPosition
-        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
-        tvQuestion?.text = question.queststion
-        tvOptionOne?.text = question.optionOne
-        tvOptionTwo?.text = question.optionTwo
-        tvOptionThree?.text = question.optionThree
-        tvOptionFour?.text = question.optionFour
+
+        defaultOptionsView()
 
         if (mCurrentPosition == mQuestionsList!!.size){
             btnSubmit?.text = "FINISH"
         }else{
             btnSubmit?.text = "SUBMIT"
         }
+        // Setting the current progress in the progressbar using the position of question
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
+        ivImage?.setImageResource(question.image)
+
+//set current question to options in the UI
+        tvQuestion?.text = question.queststion
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
     }
+    //set default options view when the new question is loaded or when the answer is reselected.
     private fun defaultOptionsView(){
         val options = ArrayList<TextView>()
         tvOptionOne?.let {
@@ -94,13 +96,13 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             option.background =ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
         }
     }
-private fun selectedOptionView(tv:TextView, selectedOptionNum: Int){
-    defaultOptionsView()
-    mSelectedOptionPosition = selectedOptionNum
-    tv.setTextColor(Color.parseColor("#363A43"))
-    tv.setTypeface(tv.typeface,Typeface.BOLD)
-    tv.background=ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
-}
+    private fun selectedOptionView(tv:TextView, selectedOptionNum: Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface,Typeface.BOLD)
+        tv.background=ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+    }
     override fun onClick(view: View?) {
         when(view?.id){
             R.id.tvOptionOne -> {
@@ -128,7 +130,50 @@ private fun selectedOptionView(tv:TextView, selectedOptionNum: Int){
                 }
             }
             R.id.btnSubmit -> {
-                //TODO: implement btn submit
+                if(mSelectedOptionPosition == 0){
+                    mCurrentPosition++
+                    //as long as we we still have questions , go to next question
+                    when{
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(this, "Congrats! you've made it to the end!",Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }else{
+                    val question = mQuestionsList?.get(mCurrentPosition-1)
+                    if(question!!.correctAnswer != mSelectedOptionPosition){
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }else{
+                        //TODO corect answer
+                    }
+                    //correct answer
+                    answerView(question.correctAnswer,R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size){
+                        btnSubmit?.text ="FINISH"
+                    }else{
+                        btnSubmit?.text ="GO TO NEXT QUESTION"
+                    }
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+    private fun answerView(answer:Int, drawableView: Int){
+        when(answer){
+            1 -> {
+                tvOptionOne?.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 -> {
+                tvOptionTwo?.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                tvOptionThree?.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4-> {
+                tvOptionFour?.background = ContextCompat.getDrawable(this, drawableView)
             }
         }
     }
